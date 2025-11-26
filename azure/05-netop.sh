9f949e8a5e9bb44a07e2fedf511f8ef569cab9ac
Costin Gamenț <costin.gament@gmail.com>
2025-11-26 13:19:48 +0100

    add network operator

---
+++ azure/05-netop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source vars
+
+echo "--- 10. Deploying Network Operator ---"
+rm -rf llm-d && git clone -b "${LLMD_VERSION}" --depth 1 https://github.com/llm-d/llm-d.git
+
+cd llm-d/docs/infra-providers/aks
+helmfile apply -f network-operator.helmfile.yaml
+cd ../../../
+
